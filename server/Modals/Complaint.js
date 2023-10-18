0d151c38d15f4675c225926fcd67e37b8c469cde
jntuh-web <147979332+jntuh-web@users.noreply.github.com>
2023-10-18 20:28:22 +0530

    Added schemas

---
+++ server/Modals/Complaint.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose");
+const Student=require("./Student")
+const complaintSchema=new mongoose.Schema({
+    complainedStudentID : {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Student',
+        required:true
+    },
+
+    complaint : {
+        type:String,
+        required:true
+    },
+
+    complaintCategory : {
+        type:String,
+        required:true
+    },
+
+    statusOfComplaint : {
+        type: String,
+        default:"Unsolved"
+    }
+})
