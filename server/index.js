405aafb698b2e0f44442fccf87edf0b5de8abfcf
tallurishiva <127586620+tallurishiva@users.noreply.github.com>
2023-10-28 16:37:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const mongoose=require("mongoose");
 
 const studentAuthRoute=require("./Routes/studentAuth")
 const adminAuthRoute=require("./Routes/adminAuth")
-
+const generatebonofide=require("./Routes/generatebonofide")
+const complaints=require("./Routes/complaints")
+const bonofides=require("./Routes/bonofides")
 app.use(express.json())
 app.use(cors());
 
@@ -19,8 +21,9 @@ mongoose.connect("mongodb://127.0.0.1:27017/Hostel",{
 
 app.use("/studentAuth",studentAuthRoute);
 app.use("/adminAuth",adminAuthRoute);
-
-
+app.use("/generatebonofide",generatebonofide)
+app.use("/complaints",complaints)
+app.use("/bonofides",bonofides)
 app.listen(5000,()=>{
     console.log("Server running")
 })
